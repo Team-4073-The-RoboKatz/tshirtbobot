b9b7195d526d28c68743faaa40636432798f10c4
dockerteen <77291580+dockerteen@users.noreply.github.com>
2022-06-20 13:41:43 -0400

    Added Placeholder CAN devices

--- src/main/java/frc/robot/hardware/constants.java
+++ src/main/java/frc/robot/hardware/constants.java
@@ -9,6 +9,9 @@ public final class constants {
     public static final int LeftBackDrive = 2;
     public static final int RightFrontDrive = 3;
     public static final int RightBackDrive = 4;
-    
+    public static final int implement1  = 5;
+    public static final int implement2  = 6;
+    public static final int implement3  = 7;
+    public static final int implement4  = 8;
     
 }
